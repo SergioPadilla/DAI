6ad63d9972b9a5659f313af7b7c04bf723ef2191
SergioPadilla <splsergiopadilla@gmail.com>
2016-10-03 22:07:25 +0200

    Practise 1 directory

---
+++ Practica1/Ejercicio1/ejercicio1.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created at 30/09/16
+__author__ = 'Sergio Padilla'
+
+"""
+from random import randint
+
+mesagges = {
+    '0': u'El número buscado es mayor',
+    '1': u'El número buscado es menor',
+    '2': u'Número máximo de intentos alcanzado, no has conseguido adivinar el número',
+    '3': u'Enhorabuena! Has acertado el número'
+}
+
+number = randint(1, 100)
+
+print 'Se ha generado un número aleatorio, ¿serás capaz de adivinarlo?'
+
+for i in range(0, 10):
+    n = raw_input('Indica un número: ')
+
+    if int(n) == number:
+        print mesagges['3']
+        break
+    elif int(n) < number:
+        print mesagges['0'] if i < 9 else mesagges['2']
+    else:
+        print mesagges['1'] if i < 9 else mesagges['2']
---
+++ Practica1/Ejercicio2/ejercicio2.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created at 30/09/16
+__author__ = 'Sergio Padilla'
+
+"""
+
