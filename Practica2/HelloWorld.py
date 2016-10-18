15f9a2e687a077e69570760252f6bf1c562c2f6e
Sergio Padilla <splsergiopadilla@gmail.com>
2016-10-19 00:20:53 +0200

    modify

--- Practica2/HelloWorld.py
+++ Practica2/HelloWorld.py
@@ -3,7 +3,7 @@ Created at 18/10/16
 __author__ = 'Sergio Padilla'
 
 """
-from flask import Flask, send_from_directory
+from flask import Flask, url_for
 app = Flask(__name__)
 
 @app.route("/")
@@ -15,10 +15,6 @@ def hello():
     <h1>Hello World!</h1>
     <img src="/static/logo.png">
     """
-
-@app.route("/<path:path>")
-def file(path):
-    return send_from_directory('static', path)
 
 if __name__ == "__main__":
     app.run()
