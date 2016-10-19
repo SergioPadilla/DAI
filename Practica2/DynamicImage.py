a97e1e8f1011aeb0872e44fe51872bf4c21c158d
Sergio Padilla <splsergiopadilla@gmail.com>
2016-10-20 00:19:27 +0200

    eercise dynamic image

--- Practica2/DynamicImage.py
+++ Practica2/DynamicImage.py
@@ -3,19 +3,28 @@ Created at 19/10/16
 __author__ = 'Sergio Padilla'
 
 """
-from flask import Flask, render_template
-import mandelbrot
+from flask import Flask, render_template, request
+from Practica2 import mandelbrot
 
 app = Flask(__name__)
 
-@app.route("/" )
+@app.route("/")
 def init():
     return render_template('dynamic_image_index.html')
 
-@app.route("/mandelbrot/<x1>/<y1>/<x2>/<y2>/<pixels>", methods=['GET'])
-def process(x1,y1,x2,y2,pixels):
+@app.route("/mandelbrot", methods=['GET'])
+def process():
+    x1 = float(request.args.get('x1'))
+    y1 = float(request.args.get('y1'))
+    x2 = float(request.args.get('x2'))
+    y2 = float(request.args.get('y2'))
+    pixels = int(request.args.get('pixels'))
     mandelbrot.renderizaMandelbrot(x1, y1, x2, y2, pixels, 100, 'fractal.png')
-    return "<img src='fractal.png'>"
+    return """
+    <body>
+        <img src='fractal.png'>
+    </body>
+    """
 
 if __name__ == "__main__":
     app.run()
--- Practica2/mandelbrot.py
+++ Practica2/mandelbrot.py
@@ -19,6 +19,7 @@
 
 from PIL import Image
 
+
 # Renderiza un fractal de Mandelbrot de (x1, y1) a (x2, y2) de ancho pixeles, iteraciones máximas y lo guarda en el fichero especificado en nombreFicheroPNG (debe tener extesión .png)
 # Valores de los parametros con los que podemos ver algo interesante:
 # x1: -1.0
@@ -29,65 +30,66 @@ from PIL import Image
 # iteraciones: 100
 
 def renderizaMandelbrot(x1, y1, x2, y2, ancho, iteraciones, nombreFicheroPNG):
-	# drawing area
-	xa = x1
-	xb = x2
-	ya = y1 + 0.000001
-	yb = y2
-	maxIt = iteraciones
-	# image size
-	imgx = ancho
-	imgy = int(abs (y2 - y1) * ancho / abs(x2 - x1));
-	
-	image = Image.new("RGB", (imgx, imgy))
+    # drawing area
+    xa = x1
+    xb = x2
+    ya = y1 + 0.000001
+    yb = y2
+    maxIt = iteraciones
+    # image size
+    imgx = ancho
+    imgy = int(abs(y2 - y1) * ancho / abs(x2 - x1))
 
-	for y in range(imgy):
-	    zy = y * (yb - ya) / (imgy - 1)  + ya
-	    for x in range(imgx):
-		zx = x * (xb - xa) / (imgx - 1)  + xa
-		z = zx + zy * 1j
-		c = z
-		for i in range(maxIt):
-		    if abs(z) > 2.0: break 
-		    z = z * z + c
-		    
-		if (i >= maxIt):
-                    image.putpixel((x, y), (0, 0, 0))
-                else:
-                    image.putpixel((x, y), (i % 4 * 64, i % 8 * 32, i % 16 * 16))
+    image = Image.new("RGB", (imgx, imgy))
 
-	image.save(nombreFicheroPNG, "PNG")
-	
+    for y in range(imgy):
+        zy = y * (yb - ya) / (imgy - 1) + ya
+        for x in range(imgx):
+            zx = x * (xb - xa) / (imgx - 1) + xa
+            z = zx + zy * 1j
+            c = z
+            for i in range(maxIt):
+                if abs(z) > 2.0: break
+                z = z * z + c
+
+            if (i >= maxIt):
+                image.putpixel((x, y), (0, 0, 0))
+            else:
+                image.putpixel((x, y), (i % 4 * 64, i % 8 * 32, i % 16 * 16))
+
+    image.save(nombreFicheroPNG, "PNG")
+
+
 # paleta es una lista de 3-tuplas con los valores RGB de cada color de la paleta
 def getColorPaleta(paleta, nColoresPaleta, color):
-        nuevoColor = color % (nColoresPaleta)
-        
-        nPuntosControlPaleta = len(paleta)
-        
-        icolor1 = float(nuevoColor) * float(nPuntosControlPaleta - 1) / float(nColoresPaleta - 1)
-        
-       # print len(paleta)
-       # print int(icolor1)
-        
-        if (int(icolor1) == len(paleta) - 1):  # Devolvemos el último color
-                return paleta[len(paleta) - 1]
-        
-        porcentajeC2 = icolor1 - int(icolor1)
-        porcentajeC1 = 1.0 - porcentajeC2
-        icolor1 = int(icolor1)
-        icolor2 = icolor1 + 1
-        
-        nr = int(paleta[icolor1][0] * porcentajeC1 + paleta[icolor2][0] * porcentajeC2)
-        ng = int(paleta[icolor1][1] * porcentajeC1 + paleta[icolor2][1] * porcentajeC2)
-        nb = int(paleta[icolor1][2] * porcentajeC1 + paleta[icolor2][2] * porcentajeC2)
-        
-        return (nr, ng, nb)
-        
-	
+    nuevoColor = color % (nColoresPaleta)
+
+    nPuntosControlPaleta = len(paleta)
+
+    icolor1 = float(nuevoColor) * float(nPuntosControlPaleta - 1) / float(nColoresPaleta - 1)
+
+    # print len(paleta)
+    # print int(icolor1)
+
+    if (int(icolor1) == len(paleta) - 1):  # Devolvemos el último color
+        return paleta[len(paleta) - 1]
+
+    porcentajeC2 = icolor1 - int(icolor1)
+    porcentajeC1 = 1.0 - porcentajeC2
+    icolor1 = int(icolor1)
+    icolor2 = icolor1 + 1
+
+    nr = int(paleta[icolor1][0] * porcentajeC1 + paleta[icolor2][0] * porcentajeC2)
+    ng = int(paleta[icolor1][1] * porcentajeC1 + paleta[icolor2][1] * porcentajeC2)
+    nb = int(paleta[icolor1][2] * porcentajeC1 + paleta[icolor2][2] * porcentajeC2)
+
+    return (nr, ng, nb)
+
+
 # Renderiza un fractal de Mandelbrot de (x1, y1) a (x2, y2) de ancho pixeles, iteraciones máximas y lo guarda en el fichero especificado en nombreFicheroPNG (debe tener extesión .png) y permitiendo especificar una paleta de colores: paleta es una lista de 3-tuplas con los valores RGB de cada color de la paleta
 
 def renderizaMandelbrotBonito(x1, y1, x2, y2, ancho, iteraciones, nombreFicheroPNG, paleta, nColoresPaleta):
-        # drawing area
+    # drawing area
         xa = x1
         xb = x2
         ya = y1 + 0.000001
