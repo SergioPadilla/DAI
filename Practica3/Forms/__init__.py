4bb90e1db7577f6178d1af7645e36699c55da5e0
Sergio Padilla <splsergiopadilla@gmail.com>
2016-10-20 01:11:36 +0200

    refactor files and jinja form

---
+++ Practica3/Forms/__init__.py
@@ -0,0 +1,5 @@
+"""
+Created at 20/10/16
+__author__ = 'Sergio Padilla'
+
+"""
--- Practica3/web.py
+++ Practica3/web.py
@@ -3,12 +3,29 @@ Created at 19/10/16
 __author__ = 'Sergio Padilla'
 
 """
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import os
+
+from Practica3.Forms.Register import RegistrationForm
+
 app = Flask(__name__)
 
-@app.route("/")
+SITE_ROOT = os.path.dirname(os.path.realpath(__file__))
+TEMPLATES_DIR = os.path.join(SITE_ROOT, '/templates/')
+
+@app.route("/", methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    form = RegistrationForm(request.form)
+    return render_template('index.html', form=form)
+
+
+    # if request.method == 'POST' and form.validate():
+    #     user = User(form.username.data, form.email.data,
+    #                 form.password.data)
+    #     db_session.add(user)
+    #     flash('Thanks for registering')
+    #     return redirect(url_for('login'))
+    # return render_template('register.html', form=form)
 
 if __name__ == "__main__":
     app.run()
