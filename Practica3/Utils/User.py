d43ecbeb1b4e55ad2b8f9feb99678a0ad778db04
Sergio Padilla <splsergiopadilla@gmail.com>
2016-10-23 16:51:42 +0200

    working session

---
+++ Practica3/Utils/User.py
@@ -0,0 +1,12 @@
+"""
+Created at 23/10/16
+__author__ = 'Sergio Padilla'
+
+"""
+
+
+class User:
+
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
--- Practica3/web.py
+++ Practica3/web.py
@@ -3,10 +3,11 @@ Created at 19/10/16
 __author__ = 'Sergio Padilla'
 
 """
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for, session
 import os
 
 from Practica3.Forms.Register import RegistrationForm
+from Practica3.Utils.User import User
 
 app = Flask(__name__)
 
@@ -16,16 +17,30 @@ TEMPLATES_DIR = os.path.join(SITE_ROOT, '/templates/')
 @app.route("/", methods=['GET', 'POST'])
 def index():
     form = RegistrationForm(request.form)
+    user = None
+    if request.method == 'POST' and form.validate():
+        session['username'] = form.username.data
+        session['password'] = form.password.data
+        user = User(form.username.data, form.password.data)
+    elif 'username' in session:
+        user = User(session['username'], session['password'])
+
+    if user:
+        return render_template('index.html', form=form, user=user.username)
+
     return render_template('index.html', form=form)
 
 
-    # if request.method == 'POST' and form.validate():
-    #     user = User(form.username.data, form.email.data,
-    #                 form.password.data)
-    #     db_session.add(user)
-    #     flash('Thanks for registering')
-    #     return redirect(url_for('login'))
-    # return render_template('register.html', form=form)
+read_file = open("credential", mode='r')
+app.secret_key = str(read_file.readline().replace('\n', ''))
+read_file.close()
+
+
+@app.route('/logout')
+def logout():
+    # remove the username from the session if it's there
+    session.pop('username', None)
+    return redirect(url_for('index'))
 
 if __name__ == "__main__":
     app.run()
