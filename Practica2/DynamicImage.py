2a58ac9a0595b3d7c6fa5722449de253bda321c3
Sergio Padilla <splsergiopadilla@gmail.com>
2016-10-28 22:14:55 +0200

    finish extra exercise practise2

--- Practica2/DynamicImage.py
+++ Practica2/DynamicImage.py
@@ -5,6 +5,8 @@ __author__ = 'Sergio Padilla'
 """
 from flask import Flask, render_template, request
 from Practica2 import mandelbrot
+import os
+import time
 
 app = Flask(__name__)
 
@@ -14,37 +16,43 @@ def init():
     return render_template('dynamic_image_index.html')
 
 
-@app.route("/mandelbrotsimple", methods=['GET'])
+@app.route("/mandelbrot", methods=['GET'])
 def process():
+    remove_old_files()
     x1 = float(request.args.get('x1'))
     y1 = float(request.args.get('y1'))
     x2 = float(request.args.get('x2'))
     y2 = float(request.args.get('y2'))
     pixels = int(request.args.get('pixels'))
-    mandelbrot.renderizaMandelbrot(x1, y1, x2, y2, pixels, 100, 'static/fractal.png')
+    paleta = request.args.get('paleta')
+    if paleta:
+        paleta = int(request.args.get('paleta'))
+        colors = [(235, 120, 43), (101, 230, 210), (15, 45, 102)] if paleta == 1 else [(255, 0, 255), (54, 255, 36),
+                                                                                       (234, 6, 24)]
+        n = int(request.args.get('n'))
+        name = '%f%f%f%f%d%d%d' % (x1, y1, x2, y2, pixels, paleta, n)
+        if not os.path.exists('static/'+name+'.png'):
+            mandelbrot.renderizaMandelbrotBonito(x1, y1, x2, y2, pixels, 100, 'static/mandelbrot/'+name+'.png', colors, n)
+    else:
+        name = '%f%f%f%f%d' % (x1, y1, x2, y2, pixels)
+        if not os.path.exists('static/' + name + '.png'):
+            mandelbrot.renderizaMandelbrot(x1, y1, x2, y2, pixels, 100, 'static/mandelbrot/'+name+'.png')
     return """
     <body>
-        <img src='/static/fractal.png'>
+        <img src='static/mandelbrot/%s.png'>
     </body>
-    """
+    """ % name
 
 
-@app.route("/mandelbrotcomplex", methods=['GET'])
-def process():
-    x1 = float(request.args.get('x1'))
-    y1 = float(request.args.get('y1'))
-    x2 = float(request.args.get('x2'))
-    y2 = float(request.args.get('y2'))
-    pixels = int(request.args.get('pixels'))
-    r = int(request.args.get('r'))
-    g = int(request.args.get('g'))
-    b = int(request.args.get('b'))
-    mandelbrot.renderizaMandelbrot(x1, y1, x2, y2, pixels, 100, 'static/fractal.png')
-    return """
-    <body>
-        <img src='/static/fractal.png'>
-    </body>
-    """
+def remove_old_files():
+    path = '/Users/sergiopadilla/github/DAI/Practica2/static/mandelbrot'
+    now = time.time()
+    seconds_day = 24*60*60
+    for f in os.listdir(path):
+        f = os.path.join(path, f)
+        if os.stat(f).st_mtime < now - seconds_day:
+            os.remove(os.path.join(path, f))
+
 
 if __name__ == "__main__":
     app.run()
--- Practica2/DynamicImagesSVG.py
+++ Practica2/DynamicImagesSVG.py
@@ -3,7 +3,7 @@ Created at 27/10/16
 __author__ = 'Sergio Padilla'
 
 """
-from flask import Flask, render_template
+from flask import Flask
 import svgwrite
 from random import randint
 
@@ -12,13 +12,38 @@ app = Flask(__name__)
 
 @app.route("/")
 def init():
-    number = randint(1, 100)
-    svg_document = svgwrite.Drawing(filename="test-svgwrite.svg", size=("800px", "600px"))
-    svg_document.add(svg_document.rect(insert=(0, 0),
-                                       size=("200px", "100px"),
-                                       stroke_width="1",
-                                       stroke="black",
-                                       fill="rgb(255,255,0)"))
+    width = str(randint(100, 200))
+    height = str(randint(50, 100))
+    r = str(randint(0, 255))
+    g = str(randint(0, 255))
+    b = str(randint(0, 255))
+    svg_document = svgwrite.Drawing(filename="static/test-svgwrite.svg", size=("800px", "600px"))
+    m = randint(0, 3)
+    if m == 1:
+        color = 'green'
+    elif m == 2:
+        color = 'black'
+    else:
+        color = 'pink'
+    n = randint(0, 3)
+    if n == 1:
+        svg_document.add(svg_document.rect(insert=(randint(100, 500), randint(100, 500)),
+                                           size=(width + "px", height + "px"),
+                                           stroke_width="1",
+                                           stroke=color,
+                                           fill="rgb(" + r + "," + g + "," + b + ")"))
+    elif n == 2:
+        svg_document.add(svg_document.ellipse(center=(randint(500, 1000), randint(500, 1000)),
+                                              r=(randint(100, 500), randint(100, 500)),
+                                              stroke_width="1",
+                                              stroke=color,
+                                              fill="rgb(" + r + "," + g + "," + b + ")"))
+    else:
+        svg_document.add(svg_document.circle(center=(randint(500, 1000), randint(500, 1000)),
+                                             r=randint(100, 500),
+                                             stroke_width="1",
+                                             stroke=color,
+                                             fill="rgb(" + r + "," + g + "," + b + ")"))
     print(svg_document.tostring())
     svg_document.save()
     return """
